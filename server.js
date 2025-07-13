72c564b4755c699812ba4fa01495e12494de38a0
Shrilakshmisr <159100816+Shrilakshmisr@users.noreply.github.com>
2025-07-13 08:00:09 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,26 +1,14 @@
-const express = require("express");
+const express = require('express');
+const path = require('path');
+const productRoutes = require('./routes/productRoutes');
+
 const app = express();
 const PORT = 3000;
 
-// Import route files
-const userRoutes = require("./routes/userRoutes");
-const productRoutes = require("./routes/productRoutes");
-const cartRoutes = require("./routes/cartRoutes");
+// Route Middleware
+app.use('/api', productRoutes);
 
-// Middleware
-app.use(express.json());
-
-// Route mounting
-app.use("/users", userRoutes);
-app.use("/products", productRoutes);
-app.use("/cart", cartRoutes);
-
-// 404 Handler
-app.use((req, res) => {
-  res.status(404).send("404 - Route Not Found");
-});
-
-// Start server
+// Start Server
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
